568cb3b9b0458cfba9277fc6ed9ad3b81eef7e63
przemour@gmail.com <przemour@gmail.com>
2018-09-15 11:56:01 +0200

    zadanie 34 wykonane

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var pierwsza = "Uwielbiam JavaScript";
+var druga = "Jestem świetnym programistą";
+
+function longer () {
+    if (pierwsza.length > druga.length) {
+        console.log(pierwsza);
+    } else {
+        console.log(druga);
+    }
+}
